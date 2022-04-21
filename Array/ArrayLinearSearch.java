2aab9be076ffd6b61b7a4fca66a1b64811985e9a
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-21 16:44:22 -0300

    Improve and clean code

--- Array/ArrayLinearSearch.java
+++ Array/ArrayLinearSearch.java
@@ -6,26 +6,34 @@ public class ArrayLinearSearch {
 
     public static void main(String[] args) {
 
-        int[] array = { 5, 10, 15, 20, 25, 30, 40 };
-        int elementToBeFound = 40;
-
-        int index = find(array, elementToBeFound);
-
+        int[] array = { 5, 10, 15, 20, 25, 30, 35, 40 };
         System.out.println("\nArray to be searched: " + Arrays.toString(array));
+
+        int elementToBeFound = 33;
         System.out.println("Searching for: " + elementToBeFound);
+
+        int index = findElement(array, elementToBeFound);
+
         System.out.println(index == -1
-                ? elementToBeFound + " not found."
-                : elementToBeFound + " is at index: " + index);
-        System.out.println("Total of attempts: " + (index + 1));
+                ? elementToBeFound + " was not found"
+                : elementToBeFound + " found at index: " + index);
 
     }
 
-    public static int find(int[] array, int elementToBeFound) {
+    public static int findElement(int[] array, int elementToBeFound) {
+
         for (int i = 0; i <= (array.length - 1); i++) {
             if (array[i] == elementToBeFound) {
+                System.out.println("Total of attempts: " + (i + 1));
                 return i;
             }
+
         }
+
+        // If code reaches here means that element was NOT FOUND
+        System.out.println("Total of attempts: " + array.length);
         return -1;
+
     }
+
 }
--- Array/ArrayLinearSearchAttempts.java
+++ Array/ArrayLinearSearchAttempts.java
@@ -6,19 +6,22 @@ public class ArrayLinearSearchAttempts {
 
     public static void main(String[] args) {
 
-        int[] array = { 5, 10, 15, 20, 25, 30, 40 };
-        int elementToBeFound = 18;
+        int[] array = { 5, 10, 15, 20, 25, 30, 35, 40 };
+        System.out.println("\nArray to be searched: " + Arrays.toString(array));
 
-        int index = search(elementToBeFound, array);
-
-        System.out.println("Array to be searched: " + Arrays.toString(array));
+        int elementToBeFound = 33;
         System.out.println("Searching for: " + elementToBeFound);
+
+        int index = findElement(array, elementToBeFound);
+
         System.out.println(index == -1
-                ? elementToBeFound + " not found"
-                : elementToBeFound + " is at index: " + index);
+                ? elementToBeFound + " was not found"
+                : elementToBeFound + " found at index: " + index);
+
     }
 
-    public static int search(int element, int[] array) {
+    public static int findElement(int[] array, int elementToBeFound) {
+
         int arrLength = array.length;
         int leftIndex = 0;
         int rightIndex = arrLength - 1;
@@ -26,21 +29,30 @@ public class ArrayLinearSearchAttempts {
         for (leftIndex = 0; leftIndex <= rightIndex;) {
 
             // Searching from LEFT to RIGHT
-            if (array[leftIndex] == element) {
-                System.out.println( "\nLEFT to RIGHT: element found with " + (leftIndex + 1) + " Attempt(s)");
+            if (array[leftIndex] == elementToBeFound) {
+                System.out.println("\nSearching LEFT-TO-RIGHT: element found with "
+                        + (leftIndex + 1) + " attempt(s)");
                 return leftIndex;
             }
 
             // Searching from RIGHT to LEFT
-            if (array[rightIndex] == element) {
-                System.out.println( "\nRIGHT to LEFT: element found with " + (arrLength - rightIndex) + " Attempt(s)");
+            if (array[rightIndex] == elementToBeFound) {
+                System.out.println("\nSearching RIGHT-TO-LEFT: element found with "
+                        + (arrLength - rightIndex) + " attempt(s)");
                 return rightIndex;
             }
+
+            // Increment left/decrement right indexes
             leftIndex++;
             rightIndex--;
+
         }
-        // The code bellow will be executed only for NOT FOUND element
-        System.out.println("\nSearching BOTH DIRECTIONS with " + leftIndex + " attempt(s) only");            
-        return -1;            
+
+        // If code reaches here means that element was NOT FOUND
+        System.out.println("\nSearching BOTH DIRECTIONS with "
+                + leftIndex + " attempt(s) only");
+        return -1;
+
     }
+
 }
