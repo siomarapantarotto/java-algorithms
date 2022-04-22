78a3298aae8f17befc18ac072eec2c439f20b837
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-22 14:23:45 -0300

    Initial commit

--- Bitwise/BitwiseOperators.java
+++ Bitwise/BitwiseOperators.java
@@ -3,12 +3,12 @@ package Bitwise;
 /**
  * By siomara.com.br on May, 2022.
  */
-public class BitwiseOperator {
+public class BitwiseOperators {
 
     public static void main(String[] args) {
         // Initialize values
-        int a = 5;
-        int b = 7;
+        // int a = 3, b = 3;
+        int a = 3, b = 2;
 
         System.out.println("\nGiven a = " + a + " and b = " + b + " then...");
         System.out.println("binary  a = " + Integer.toBinaryString(a));
@@ -18,23 +18,23 @@ public class BitwiseOperator {
         System.out.println("a & b \t==> " + Integer.toBinaryString(a & b)
                 + "\t= " + (a & b));
 
+        // combined operators for shorthand assignment
+        System.out.println("a &= b \t==> " + Integer.toBinaryString(a &= b)
+                + "\t= " + (a &= b));
+
         // bitwise OR
         System.out.println("a | b \t==> " + Integer.toBinaryString(a | b)
                 + "\t= " + (a | b));
+
+        // combined operators for shorthand assignment
+        System.out.println("a &= b \t==> " + Integer.toBinaryString(a |= b)
+                + "\t= " + (a |= b));
 
         // bitwise XOR
         System.out.println("a ^ b \t==> " + Integer.toBinaryString(a ^ b)
                 + "\t= " + (a ^ b));
 
-        // combined in assignment operator providing shorthand assignment
-        // a &= b is same as a = a & b
-        System.out.println("a &= b \t==> " + Integer.toBinaryString(a &= b)
-                + "\t= " + (a &= b));
-
         // bitwise NOT
-        // ~0101 = 1010
-        // will give 2's complement of 1010 = -6
-        //System.out.println("~a = " + Integer.toBinaryString(~a));
         System.out.println("~a = " + ~a);
     }
 }
---
+++ Bitwise/SingleElement.java
@@ -0,0 +1,26 @@
+package Bitwise;
+
+// Find the array element that occur only once.
+class SingleElement {
+	// Driver method.
+	public static void main(String args[]) {
+		int array[] = { 0, 3, 1, 3, 0, 5, 1, 3, 1, 0 };
+		System.out.println("The single element is: " + getSingleElement(array));
+	}
+
+	// Method to find the element that occur only once
+	static int getSingleElement(int array[]) {
+		int arrLength = array.length;
+		int ones = 0, twos = 0;
+		int common_bit_mask;
+
+		for (int i = 0; i < arrLength; i++) {
+			twos = twos | (ones & array[i]);
+			ones = ones ^ array[i];
+			common_bit_mask = ~(ones & twos);
+			ones = ones & common_bit_mask;
+			twos = twos & common_bit_mask;
+		}
+		return ones;
+	}
+}
