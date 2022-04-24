fb24de0b411b4b051df22d8e7bc0431c37738404
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-23 21:27:01 -0300

    Initial commit

--- Bitwise/SubsetSum.java
+++ Bitwise/SubsetSum.java
@@ -13,8 +13,8 @@ public class SubsetSum {
 
         // Set index 0 for both arrays equal to 1.
         a[0] = b[0] = 1;
-        System.out.println("a = " + Arrays.toString(a)); // a = [1, 0, 0]
-        System.out.println("b = " + Arrays.toString(b)); // b = [1, 0, 0]
+        // System.out.println("a = " + Arrays.toString(a)); // a = [1, 0, 0]
+        // System.out.println("b = " + Arrays.toString(b)); // b = [1, 0, 0]
 
         // Loop
         System.out.println("Loop / n = " + n + " / (int i = 1; i < n; i++)");
@@ -22,19 +22,19 @@ public class SubsetSum {
             a[i] = a[i - 1] + b[i - 1];
             b[i] = a[i - 1];
 
-            System.out.println();
-            System.out.println("a[i] = a[" + i + "] = " + a[i] + " and  "
-                    + "b[i] = b[" + i + "] = " + b[i]);
-            System.out.println();
-            System.out.println("a[" + i + "] binary = " + Integer.toBinaryString(a[i]));
-            System.out.println("b[" + i + "] binary = " + Integer.toBinaryString(b[i]));
-            System.out.println("a = " + Arrays.toString(a));
-            System.out.println("b = " + Arrays.toString(b));
-            System.out.println("------------------------------------------");
+            // System.out.println();
+            // System.out.println("a[i] = a[" + i + "] = " + a[i] + " and "
+            // + "b[i] = b[" + i + "] = " + b[i]);
+            // System.out.println();
+            // System.out.println("a[" + i + "] binary = " + Integer.toBinaryString(a[i]));
+            // System.out.println("b[" + i + "] binary = " + Integer.toBinaryString(b[i]));
+            // System.out.println("a = " + Arrays.toString(a));
+            // System.out.println("b = " + Arrays.toString(b));
+            // System.out.println("------------------------------------------");
 
         }
-        System.out.println("a[n - 1] + b[n - 1]");
-        System.out.println("n = " + n);
+        // System.out.println("a[n - 1] + b[n - 1]");
+        // System.out.println("n = " + n);
         return a[n - 1] + b[n - 1];
     }
 
