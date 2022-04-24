e247cc9fd4ee1d2309658ed667701c54988296e1
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-23 23:51:21 -0300

    Initial commit

--- Bitwise/AddOne.java
+++ Bitwise/AddOne.java
@@ -12,17 +12,5 @@ public class AddOne {
     }
 }
 
-/**
 // This code is contributed by Smitha Dinesh Semwal
 
-Time Complexity: O(1)
-Auxiliary Space: O(1)
-We know that the negative number is represented in 2’s complement
-form on most of the architectures. We have the following lemma
-hold for 2’s complement representation of signed numbers.
-Say, x is numerical value of a number, then
-~x = -(x+1) [ ~ is for bitwise complement ]
-(x + 1) is due to the addition of 1 in 2’s complement conversion
-To get (x + 1) apply negation once again.
-So, the final expression becomes (-(~x)). 
-*/
