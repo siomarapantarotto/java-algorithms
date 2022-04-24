6f7ed0edbe3c384ebabaee64b06b6850cc84d51e
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-23 23:57:24 -0300

    Initial commit

---
+++ Bitwise/SubtractOne.java
@@ -0,0 +1,18 @@
+package Bitwise;
+
+public class SubtractOne {
+
+	static int subtractOne(int x)
+	{
+		return ((x << 1) + (~x));
+	}
+
+	/* Driver code*/
+	public static void main(String[] args)
+	{
+		System.out.printf("%d", subtractOne(13));
+	}
+}
+
+// This code has been contributed by 29AjayKumar
+
