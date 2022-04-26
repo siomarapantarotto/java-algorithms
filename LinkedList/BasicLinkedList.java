9e43268f410848ea2d2d13c6b75bef2d728b3c5d
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-25 22:06:35 -0300

    Implement addFirst(), addLast(), empty remove()

--- LinkedList/BasicLinkedList.java
+++ LinkedList/BasicLinkedList.java
@@ -2,6 +2,11 @@ package LinkedList;
 
 import java.util.LinkedList;
 
+/**
+ * The LinkedList class implements the List interface
+ * as well as the Deque interface.
+ * Sintax: LinkedList<?> linkedListName = new LinkedList<?>();
+ */
 public class BasicLinkedList {
 
     public static void main(String[] args) {
@@ -19,12 +24,25 @@ public class BasicLinkedList {
 
         linkedList.addFirst(1);
         System.out.println(linkedList);
-        
+
         linkedList.addLast(50);
         System.out.println(linkedList);
 
+        linkedList.remove();
+        System.out.println("Just an empty 'remove()' " + linkedList);
+
+        System.out.println("Lisked List size is " + linkedList.size());
+
         linkedList.add(3, 33);
         System.out.println(linkedList);
+
+        System.out.println(linkedList.contains(33)
+                ? "Linked List contains 33"
+                : "Linked List does not contain 33");
+
+        System.out.println(linkedList.contains(44)
+                ? "Linked List contains 44"
+                : "Linked List does not contain 44");
 
     }
 
