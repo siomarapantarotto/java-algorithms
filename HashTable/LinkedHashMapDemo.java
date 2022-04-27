1bc32e53a02de4d5d75ccfb403977225179571f5
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-26 23:53:39 -0300

    Update input values

--- HashTable/LinkedHashMapDemo.java
+++ HashTable/LinkedHashMapDemo.java
@@ -6,11 +6,11 @@ public class LinkedHashMapDemo
    public static void main(String args[])
    {
        LinkedHashMap<Integer, String> lhm= new LinkedHashMap<Integer, String>();
-       lhm.put(10, "Chaitanya");
-       lhm.put(1, "Ajeet");
-       lhm.put(11, "Test");
-       lhm.put(9, "Demo");
-       lhm.put(3, "Anuj");
+       lhm.put(10, "Zelia");
+       lhm.put(1, "Beto");
+       lhm.put(11, "Tania");
+       lhm.put(9, "Daniel");
+       lhm.put(3, "Amanda");
  
        // Get a set of the entries
        Set set = lhm.entrySet();
