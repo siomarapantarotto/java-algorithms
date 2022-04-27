e9a47170b71e76db2dd23e9e46cfc2b339bc294f
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-27 00:33:13 -0300

    Rename class

--- HashTable/SimpleHashTable.java
+++ HashTable/SimpleHashTable.java
@@ -4,32 +4,32 @@ import java.util.Arrays;
 /**
  * by siomara.com.br on May, 21 of 2022
  */
-public class SimpleHashTable {
+public class SimpleHashtable {
     
     String[] array;
     //int arraySize;
     
     public static void main(String[] args) {
-        SimpleHashTable simpleHashTable = new SimpleHashTable(20);
+        SimpleHashtable simpleHashtable = new SimpleHashtable(20);
         
         // Create and print empty array hash table before hashing
         System.out.println("\nEmpty array hash table:");
-        System.out.println(Arrays.toString(simpleHashTable.array));
+        System.out.println(Arrays.toString(simpleHashtable.array));
         
         // Do not pass duplicate values as elements to be added
         String[] elementsToAdd = { "1", "3", "5", "6", "9", "12", "15", "17", "19" };
 
         // Call hash function that will place each element in its index position
-        simpleHashTable.hashFunction(elementsToAdd, simpleHashTable.array);
+        simpleHashtable.hashFunction(elementsToAdd, simpleHashtable.array);
         
         // Print filled array hash table after hashing
         System.out.println("\nFilled array hash table:");
-        System.out.println(Arrays.toString(simpleHashTable.array));
+        System.out.println(Arrays.toString(simpleHashtable.array));
     }
 
     // Constructor
     // Simulate an empty array hash table with [] in all positions 
-    SimpleHashTable(int arraySize) {
+    SimpleHashtable(int arraySize) {
         this.array = new String[arraySize];
         Arrays.fill(this.array, "[]");
     }
