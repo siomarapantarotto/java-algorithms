b96c461c232a7534c87a70e34fdc22a3e974d8e3
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-28 01:02:03 -0300

    Rename variables

--- BinarySearchTree/BasicBinarySearchTree.java
+++ BinarySearchTree/BasicBinarySearchTree.java
@@ -4,29 +4,30 @@ import java.util.*;
 
 public class BasicBinarySearchTree {
 
-    public static void main (String args[]) {
-        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 20 };
-        searchFor(array, 6);
-    }
+    public static int binarySearchRecursive(int[] array, int elementToBeFound, int left, int right) {
 
-    public static int searchFor(int[] array, int number) {
-        int arrayLength = array.length;
-        int leftIndex = 0;
-        int rightIndex = arrayLength - 1;
-        int midIndex = arrayLength/2;
-
-        if (leftIndex >= rightIndex) {
+        if (left > right) {
             return -1;
         }
 
-        if (array[midIndex] == number) {
-            return midIndex;
-        } else  if {
-            return searchFor(array, number);
-        } else {
-            return searchFor(array, number);
+        int mid = left + (right - left) / 2;
+        if (array[mid] == elementToBeFound) {
+            return mid;
         }
 
+        if (elementToBeFound < array[mid]) {
+            return binarySearchRecursive(array, elementToBeFound, left, mid - 1);
+        } else {
+            return binarySearchRecursive(array, elementToBeFound, mid + 1, right);
+        }
     }
-    
+
+    public static void main(String args[]) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 20 };
+        int elementToBeFound = 20;
+        int index = binarySearchRecursive(array, elementToBeFound, 0, 9);
+        System.out.println(index == -1
+                ? elementToBeFound + " was not found"
+                : elementToBeFound + " was found at index " + index);
+    }
 }
