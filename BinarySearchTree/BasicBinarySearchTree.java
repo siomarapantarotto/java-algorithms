1a0a1b2513c8144f1a48f579c319f7f156658da5
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-27 23:41:55 -0300

    Initial commit

---
+++ BinarySearchTree/BasicBinarySearchTree.java
@@ -0,0 +1,30 @@
+package BinarySearchTree;
+
+import java.util.*;
+
+public class BasicBinarySearchTree {
+
+    public static void main (String args[]) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 20 };
+        searchFor(array, 6);
+    }
+
+    public static int searchFor(int[] array, int number) {
+        int arrayLength = array.length;
+        int leftIndex = 0;
+        int rightIndex = arrayLength - 1;
+        int midIndex = arrayLength/2;
+
+        if (leftIndex >= rightIndex) {
+            return -1;
+        }
+
+        int[] arrayLeft = 
+        int[] arrayRight =
+
+
+        return -1;
+
+    }
+    
+}
