9fab4bc79b451119493b859fd23fb147e8a67c4b
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-27 22:47:10 -0300

    Add documentation and clean code

---
+++ HashTable/HashtableDemo.java
@@ -0,0 +1,32 @@
+package Hashtable;
+
+import java.util.Iterator;
+import java.util.Hashtable;
+import java.util.Set;
+
+public class HashtableDemo {
+
+    public static void main (String[] args) {
+
+        String key;
+
+        Hashtable<String, String> hashtable = new Hashtable<>();
+
+        hashtable.put("A", "blue");
+        hashtable.put("B", "green");
+        hashtable.put("C", "red");
+        hashtable.put("D", "black");
+        hashtable.put("E", "Yellow");
+
+        Set<String> keys = hashtable.keySet();
+        Iterator<String> iterator = keys.iterator();
+        
+        System.out.println("\nHashtable contains:");
+        while(iterator.hasNext()) {
+            key = iterator.next();
+            System.out.println("Key: " + key + "\tValue: " + hashtable.get(key));
+        }
+
+    }
+
+}
