29f95fa90f2594a9af55672728f34141006994c4
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-29 06:08:20 -0300

    Add containKeys(), contains(), get(), hashCode()

--- HashTable/HashtableAddElements.java
+++ HashTable/HashtableAddElements.java
@@ -7,8 +7,7 @@ import java.io.*;
 import java.util.*;
 
 class HashtableAddElements {
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		// No need to mention the Generic type twice
 		Hashtable<Integer, String> ht1 = new Hashtable<>();
 
@@ -25,10 +24,28 @@ class HashtableAddElements {
 		ht2.put(6, "six");
 
 		// Print mappings to the console
-		System.out.println("\nMappings of ht1 : " + ht1);
-		System.out.println("\nMappings of ht2 : " + ht2);
+		System.out.println("Mappings of ht1 single Generics : " + ht1);
+		System.out.println("Mappings of ht2 using Generics  : " + ht2);
 
+		System.out.println(ht1.containsKey(1)
+				? "HT1 contains key : " + 1
+				: "HT1 Does noy contain key : " + 1);
+		System.out.println(ht1.containsKey(7)
+				? "HT1 contains key : " + 7
+				: "HT1 does not contain key : " + 7);
+
+		System.out.println(ht1.contains("one")
+				? "HT1 contains value : one"
+				: "HT1 Does not contain value : one");
+		System.out.println(ht1.contains("seven")
+				? "HT1 contains value : seven"
+				: "HT1 does not contain value : seven");
+
+		System.out.println("HT2 get key 1 : " + ht2.get(1));
+
+		System.out.println("HT2 hashCode() : " + ht2.hashCode());
+
+		
 
 	}
 }
-
