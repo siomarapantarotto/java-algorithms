089dbdca595992fb8ae598979c2d460be634c42f
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-29 08:01:41 -0300

    Check hastables equality

--- HashTable/HashtableAddElements.java
+++ HashTable/HashtableAddElements.java
@@ -27,6 +27,7 @@ class HashtableAddElements {
 		System.out.println("Mappings of ht1 single Generics : " + ht1);
 		System.out.println("Mappings of ht2 using Generics  : " + ht2);
 
+		// Check if hashtable contains key
 		System.out.println(ht1.containsKey(1)
 				? "HT1 contains key : " + 1
 				: "HT1 Does noy contain key : " + 1);
@@ -34,6 +35,7 @@ class HashtableAddElements {
 				? "HT1 contains key : " + 7
 				: "HT1 does not contain key : " + 7);
 
+		// Check if hashtable contains key/values
 		System.out.println(ht1.contains("one")
 				? "HT1 contains value : one"
 				: "HT1 Does not contain value : one");
@@ -41,9 +43,17 @@ class HashtableAddElements {
 				? "HT1 contains value : seven"
 				: "HT1 does not contain value : seven");
 
+		// Get key of an element
 		System.out.println("HT2 get key 1 : " + ht2.get(1));
+		System.out.println("HT2 get key 6 : " + ht2.get(6));
 
+		// Get hash code 
+		System.out.println("HT1 hashCode() : " + ht1.hashCode());
 		System.out.println("HT2 hashCode() : " + ht2.hashCode());
+
+		// check hastables equality
+		boolean isequal = ht1.equals(ht2);
+		System.out.println("Are two tables equal : "+isequal); 
 
 		
 
