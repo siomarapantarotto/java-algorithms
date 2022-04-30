f60f372bd8c8ae449788a845844fe52ac5c6a8df
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-30 07:40:49 -0300

    Initial commit

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+public class Palindrome {
+
+    public static void main (String[] args) {
+        System.out.println(isPalindrome("kayak"));
+    }
+
+    public static boolean isPalindrome(String input) {
+        if (input.length() == 0 || input.length() == 1) {
+            return true;
+        }
+
+        if (input.charAt(0) == input.charAt(input.length() - 1)) {
+             return isPalindrome(input.substring(1, input.length() -1));
+        }
+
+        // Here different chars were found.
+        return false;
+    }
+}
