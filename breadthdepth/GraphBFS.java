2c39f67b358e1622abfd728c21bd237d8572009f
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-30 10:09:26 -0300

    Comment error line

--- breadthdepth/GraphBFS.java
+++ breadthdepth/GraphBFS.java
@@ -9,7 +9,7 @@ class GraphBFS {
 
     GraphBFS(int v) {
         this.V = v;
-        this.adj = new LinkedList[v];
+        //this.adj = new LinkedList[v];
         for (int i = 0; i < v; i++) {
             adj[i] = new LinkedList<>();
         }
