31915cc9b87160a1fef68c65552dc1820694a9b9
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-30 07:10:49 -0300

    Initial commit

---
+++ ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+
+    public static String reverseString(String input) {
+        if (input.equals("")) {
+            return "";
+        }
+        return reverseString( input.substring(1) ) + input.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("siomara"));
+    }
+
+}
