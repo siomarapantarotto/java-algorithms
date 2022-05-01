2ce16c3279571fbe05a1ddc88cceecc813bc87b7
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-04-30 21:44:51 -0300

    Improve main function result

--- recursive/ReverseString.java
+++ recursive/ReverseString.java
@@ -1,15 +1,25 @@
 package recursive;
+
 public class ReverseString {
 
     public static String reverseString(String input) {
+
+        // base case
         if (input.equals("")) {
             return "";
         }
-        return reverseString( input.substring(1) ) + input.charAt(0);
+
+        // reverse input string recursively
+        return reverseString(input.substring(1)) + input.charAt(0);
     }
 
     public static void main(String[] args) {
-        System.out.println(reverseString("siomara"));
+
+        // System.out.println(reverseString("siomara"));
+        String strToBeReversed = "Hello Charlie Brown!";
+        System.out.println("\nThe reverse of '" + strToBeReversed + "' is: "
+                + reverseString(strToBeReversed));
+
     }
 
 }
