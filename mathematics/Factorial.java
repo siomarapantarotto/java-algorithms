5e611e4ea1ca384da55be2f3de996d32c1e28e27
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-01 18:05:10 -0300

    Add optimization block

--- mathematics/Factorial.java
+++ mathematics/Factorial.java
@@ -1,13 +1,27 @@
 package mathematics;
 
 public class Factorial {
-    
-    static int factorial( int n ) {
-        if (n != 0)  // termination condition
-            return n * factorial(n-1); // recursive call
+
+    static int factorial(int n) {
+        if (n != 0) // termination condition
+            return n * factorial(n - 1); // recursive call
         else
             return 1;
     }
+
+    //////////////////////////////////////////////////////
+    // Optmization in some languages but not JAVA :-(
+    // static int factorial(int n) {
+    // return tailFactorial(n, 1); // recursive call
+    // }
+    //
+    // static int tailFactorial(int n, int multiplier) {
+    // if (n > 0) { // termination condition
+    // return tailFactorial(n - 1, n * multiplier); // recursive call
+    // }
+    // return multiplier;
+    // }
+    //////////////////////////////////////////////////////
 
     public static void main(String[] args) {
         int number = 10, result;
