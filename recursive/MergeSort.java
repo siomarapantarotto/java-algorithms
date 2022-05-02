0b6f518cd53467d260808df92b8f43da60260305
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-02 10:55:27 -0300

    Initial commit

---
+++ recursive/MergeSort.java
@@ -0,0 +1,64 @@
+package recursive;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void mergeSort(int[] data, int start, int end) {
+
+        if (start < end) {
+            int mid = (start + end) / 2;
+            mergeSort(data, start, mid);
+            mergeSort(data, mid + 1, end);
+            merge(data, start, mid, end);
+        }
+    }
+
+    public static void merge(int[] data, int start, int mid, int end) {
+
+        int temp[] = new int[end - start + 1];
+
+        int i = start, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= end) {
+            if (data[i] < data[j]) {
+                temp[k] = data[i];
+                i++;
+                k++;
+            } else {
+                temp[k] = data[j];
+                j++;
+                k++;
+            }
+        }
+
+        // add the rest of values from left/right sub-array into result
+        while (i <= mid) {
+            temp[k] = data[i];
+            i++;
+            k++;
+        }
+
+        while (j <= end) {
+            temp[k] = data[j];
+            j++;
+            k++;
+        }
+
+        for (i = start; i <= end; i++) {
+            data[i] = temp[i - start];
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] data = new int[] { -5, 20, 10, 3, 2, 0 };
+
+        System.out.println(Arrays.toString(data));
+        mergeSort(data, 0, data.length - 1);
+        System.out.println(Arrays.toString(data));
+
+    }
+
+}
