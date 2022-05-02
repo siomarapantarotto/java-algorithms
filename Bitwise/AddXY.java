ea4ebc2415b14515b248e10a3d2234c626896c5a
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-02 10:56:24 -0300

    Clean code

--- Bitwise/AddXY.java
+++ Bitwise/AddXY.java
@@ -2,35 +2,24 @@ package bitwise;
 
 public class AddXY {
     // Main drive
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("\nAddXY is: ");
-        System.out.println(addXY(12, 20));
+        System.out.println(addXY(0, 20));
         System.out.println("\nAddYX is: ");
         System.out.println(addYX(12, 20));
     }
 
-    // Add x + y
-    public static int addXY(int x, int y) {        
+    // Add x + y method 1
+    public static int addXY(int x, int y) {
         return (y == 0 ? x : addXY(x ^ y, (x & y) << 1));
     }
 
-    public static int addYX(int x, int y)
-    {
+    // Add x + y method 2
+    public static int addYX(int x, int y) {
         // Iterate till there is no carry
-        while (y != 0)
-        {
-            // carry now contains common
-            // set bits of x and y
+        while (y != 0) {
             int carry = x & y;
- 
-            // Sum of bits of x and
-            // y where at least one
-            // of the bits is not set
             x = x ^ y;
- 
-            // Carry is shifted by
-            // one so that adding it
-            // to x gives the required sum
             y = carry << 1;
         }
         return x;
