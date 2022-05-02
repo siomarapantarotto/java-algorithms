b311c1c6e8e7b60a2818951f01f6105997762326
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-02 14:18:00 -0300

    First commit

--- linkedlist/MergeTwoSortedLists.java
+++ linkedlist/MergeTwoSortedLists.java
@@ -1,5 +1,23 @@
 package linkedlist;
 
+import linkedlist.LinkedList.Node;
+
 public class MergeTwoSortedLists {
-    
+
+    public static Node sortedMerge(Node A, Node B) {
+
+        if (A == null)
+            return B;
+        if (B == null)
+            return A;
+
+        if (A.data < B.data) {
+            A.next = sortedMerge(A.next, B);
+            return A;
+        } else {
+            B.next = sortedMerge(A, B.next);
+            return B;
+        }
+    }
+
 }
