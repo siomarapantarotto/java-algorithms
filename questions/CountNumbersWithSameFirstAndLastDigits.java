0fe601858756b4ba67a044b7080e6c52f351959a
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-03 21:11:16 -0300

    Initial commit

---
+++ questions/CountNumbersWithSameFirstAndLastDigits.java
@@ -0,0 +1,13 @@
+package questions;
+
+public class CountNumbersWithSameFirstAndLastDigits {
+
+    public static void main(String[] args) {
+
+    }
+}
+// TODO: Oh boy! How can I solve this?
+// Given a range of numbers count the numbers
+// which have same first and last digits.
+// Ex: between 7 and 20 such numbers are
+// 8, 9 and 11
---
+++ questions/ReverseString.java
@@ -0,0 +1,18 @@
+package questions;
+
+// by siomara.com.br on May, 3 of 2022.
+public class ReverseString {
+
+    public static String reverseString(String input) {
+        if (input == "") {
+            return ""; // base case
+        }
+        return reverseString(input.substring(1)) + input.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        String input = "siomara.com.br";
+        System.out.println(reverseString(input));
+    }
+
+}
