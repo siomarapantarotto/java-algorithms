31e97aa23efc2a0d4d7c6f76ccada94fbab6c450
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-05 18:16:16 -0300

    Shrink code

--- sort/mergesort/MyMergeSortPractice.java
+++ sort/mergesort/MyMergeSortPractice.java
@@ -7,7 +7,7 @@ public class MyMergeSortPractice {
     public static void mergeSort(int[] data, int start, int end) {
 
         if (start < end) {
-            int mid = (start + end) / 2;
+            int mid = (start + end) / 2; // find middle
             mergeSort(data, start, mid);
             mergeSort(data, mid + 1, end);
             merge(data, start, mid, end);
@@ -20,24 +20,21 @@ public class MyMergeSortPractice {
         int i = start, j = mid + 1, k = 0;
 
         while (i <= mid && j <= end) { // double condition
-            if (data[i] < data[j]) {
+            if (data[i] < data[j])
                 temp[k++] = data[i++];
-            } else {
+            else
                 temp[k++] = data[j++];
-            }
         }
 
-        while (i <= mid) {
+        while (i <= mid)
             temp[k++] = data[i++];
-        }
 
-        while (j <= end) {
+        while (j <= end)
             temp[k++] = data[j++];
-        }
 
-        for (i = start; i <= end; i++) {
+        for (i = start; i <= end; i++)
             data[i] = temp[i - start];
-        }
+
     }
 
     public static void main(String[] args) {
