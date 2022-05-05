371a182e3b0ec341b3b367ebb7b99123814e39af
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-05 18:31:24 -0300

    Shrink code and add alerts

--- sort/mergesort/MyMergeSortPractice.java
+++ sort/mergesort/MyMergeSortPractice.java
@@ -16,15 +16,14 @@ public class MyMergeSortPractice {
 
     public static void merge(int[] data, int start, int mid, int end) {
 
-        int[] temp = new int[end - start + 1];
+        int[] temp = new int[end - start + 1]; // set correct size
         int i = start, j = mid + 1, k = 0;
 
-        while (i <= mid && j <= end) { // double condition
+        while (i <= mid && j <= end) // double condition
             if (data[i] < data[j])
                 temp[k++] = data[i++];
             else
                 temp[k++] = data[j++];
-        }
 
         while (i <= mid)
             temp[k++] = data[i++];
@@ -33,12 +32,12 @@ public class MyMergeSortPractice {
             temp[k++] = data[j++];
 
         for (i = start; i <= end; i++)
-            data[i] = temp[i - start];
+            data[i] = temp[i - start]; // attention to temp index
 
     }
 
     public static void main(String[] args) {
-        int[] data = new int[] { 40, 30, 60, 79, 32, 50, 100 };
+        int[] data = new int[] { 40, 30, 60, 458, 79, 32, 50, 100 };
 
         System.out.println("Before Merge Sort: " + Arrays.toString(data));
         mergeSort(data, 0, data.length - 1);
