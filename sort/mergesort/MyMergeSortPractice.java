479e3f2f46319edd1ac06ccc2c0f517be0106349
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-05 18:05:40 -0300

    Optimize and clean code

--- sort/mergesort/MyMergeSortPractice.java
+++ sort/mergesort/MyMergeSortPractice.java
@@ -12,54 +12,39 @@ public class MyMergeSortPractice {
             mergeSort(data, mid + 1, end);
             merge(data, start, mid, end);
         }
-
     }
 
     public static void merge(int[] data, int start, int mid, int end) {
 
         int[] temp = new int[end - start + 1];
-
         int i = start, j = mid + 1, k = 0;
 
-        while (i <= mid && j <= end) {
-
+        while (i <= mid && j <= end) { // double condition
             if (data[i] < data[j]) {
-                temp[k] = data[i];
-                i++;
-                k++;
+                temp[k++] = data[i++];
             } else {
-                temp[k] = data[j];
-                j++;
-                k++;
+                temp[k++] = data[j++];
             }
         }
 
         while (i <= mid) {
-            temp[k] = data[i];
-            i++;
-            k++;
+            temp[k++] = data[i++];
         }
 
         while (j <= end) {
-            temp[k] = data[j];
-            j++;
-            k++;
+            temp[k++] = data[j++];
         }
 
         for (i = start; i <= end; i++) {
             data[i] = temp[i - start];
         }
-
     }
 
     public static void main(String[] args) {
-
         int[] data = new int[] { 40, 30, 60, 79, 32, 50, 100 };
 
-        System.out.println(Arrays.toString(data));
+        System.out.println("Before Merge Sort: " + Arrays.toString(data));
         mergeSort(data, 0, data.length - 1);
-        System.out.println(Arrays.toString(data));
-
+        System.out.println("After Merge Sort : " + Arrays.toString(data));
     }
-
 }
