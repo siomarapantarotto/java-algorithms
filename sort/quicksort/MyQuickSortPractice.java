a23cf83e3e3961f7ae827ccfc80113e7c5a61b27
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-05 20:40:04 -0300

    Initial commit

---
+++ sort/quicksort/MyQuickSortPractice.java
@@ -0,0 +1,22 @@
+package sort.quicksort;
+
+public class MyQuickSortPractice {
+
+    public static void quickSort(int[] data, int leftIndex, int rightIndex) {
+
+        
+
+
+        
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] data = new int[] { 3, 7, 2, 5, 8, 9, 10, 1, 4 };
+        System.out.println(data);
+        quickSort(data, 0, data.length - 1);
+        System.out.println(data);
+    }
+
+}
