78a02c9bcd3c98a86e122591eea7302654060233
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-09 04:48:18 -0300

    Initial commit

---
+++ challenge/string/CountCamelCaseWords.java
@@ -0,0 +1,27 @@
+package challenge.string;
+
+public class CountCamelCaseWords {
+
+    public static int countCamelCaseWords(String input) {
+
+        int count = 0;
+
+        if (!input.isEmpty()) {
+            count = 1;
+            for (int i = 1; i < input.length() - 1; i++) {
+                // if (input.charAt(i) >= 65 && input.charAt(i) <= 90)
+                if (Character.isUpperCase(input.charAt(i)))
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String input = "oneTwoThreeFourFiveSixS"; // return 6
+        // String input = ""; // return 0
+        int count = countCamelCaseWords(input);
+        System.out.println(count);
+    }
+
+}
