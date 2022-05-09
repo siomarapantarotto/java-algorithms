803e67f18ffeedf66f1cecc6ba1f566aedbde38c
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-09 00:19:06 -0300

    Initial commit

---
+++ questions/PalindromeForLoopOnly.java
@@ -0,0 +1,23 @@
+package questions;
+
+public class PalindromeForLoopOnly {
+
+    public static boolean isPalindrome(String input) {
+
+        String reversed = "";
+        int inputLengthMinusOne = input.length() - 1;
+
+        for (int i = inputLengthMinusOne; i >= 0; i--) // inputLength - 1 is a must
+            reversed += input.charAt(i);
+
+        if (reversed.equals(input)) // string uses .equals for comparison
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        String input = "racecar";
+        System.out.println(isPalindrome(input));
+    }
+}
