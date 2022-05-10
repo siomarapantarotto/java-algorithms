391cac780e50444212cc4ddef353b5e35ed39534
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-10 04:16:22 -0300

    Shrink and clean code

--- challenge/string/BalancedParenthesesChecker.java
+++ challenge/string/BalancedParenthesesChecker.java
@@ -1,38 +1,57 @@
 package challenge.string;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 public class BalancedParenthesesChecker {
 
-    public static int checkForBalancedParenthesis(String input) {
+    public static boolean checkForBalancedParenthesis(String input) {
 
         Stack<Character> stack = new Stack<>();
+        Set<Character> set = new HashSet<Character>(
+                Arrays.asList('(', ')', '[', ']', '{', '}'));
 
         for (char currentChar : input.toCharArray()) {
 
-            if (currentChar == '(' || currentChar == '[' || currentChar == '{')
+            if (!set.contains(currentChar))
+                continue;
+
+            if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
                 stack.push(currentChar);
+                continue;
+            }
+
+            if (stack.isEmpty())
+                return false;
 
             if (currentChar == ')' && stack.peek() == '(')
                 stack.pop();
+            else if (currentChar == ')' && stack.peek() != '(')
+                return false;
 
             if (currentChar == ']' && stack.peek() == '[')
                 stack.pop();
+            else if (currentChar == ']' && stack.peek() != '[')
+                return false;
 
             if (currentChar == '}' && stack.peek() == '{')
                 stack.pop();
+            else if (currentChar == '}' && stack.peek() != '{')
+                return false;
+                
         }
 
-        if (stack.isEmpty())
-            return 0;
-        else
-            return -1;
+        // here is balanced for sure
+        return (stack.isEmpty());
+
     }
 
     public static void main(String[] args) {
-        String input = "{ [ (1 + 1) * 2 / 2] -3 }";
-        int balancedParenthesis = checkForBalancedParenthesis(input);
-        System.out.println((balancedParenthesis == -1)
+        String input = "{ [ ()(1 + 1) * 2 / 2 []] - 3 } {}";
+        boolean balancedParenthesis = checkForBalancedParenthesis(input);
+        System.out.println((balancedParenthesis == false)
                 ? "Not balanced"
                 : "balanced");
         ;
