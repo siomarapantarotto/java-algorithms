037887b2e78989538ebb28d5d0116e4229fcfb7c
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-11 00:48:29 -0300

    Initial commit @Test

---
+++ linkedlist/LinkedListToArrayConverter2.java
@@ -0,0 +1,38 @@
+package linkedlist;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * Convert Linked List to Array
+ */
+public class LinkedListToArrayConverter2 {
+
+    // Test Driven Development by Aseem Jain
+    @Test
+    public void test() {
+
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("first");
+        ll.add("second");
+        ll.add("third");
+        ll.add("last");
+
+        String[] arr = convert(ll);
+        assert 4 == arr.length;
+        assert "first" == arr[0];
+        assert "last" == arr[3];
+
+    }
+
+    String[] convert(LinkedList<String> ll) {
+        String[] arr = new String[ll.size()];
+        int i = 0;
+        for (String s : ll) {
+            arr[i++] = s;
+        }
+        return arr;
+    }
+
+}
