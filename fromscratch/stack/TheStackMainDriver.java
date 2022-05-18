832329bc50e6a188b41439e2fa44bb6e9bc54e47
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-17 22:38:35 -0300

    Add comments and clean code

--- fromscratch/stack/TheStackMainDriver.java
+++ fromscratch/stack/TheStackMainDriver.java
@@ -11,74 +11,75 @@ public class TheStackMainDriver {
         String strValue = "";
 
         //////////////////////////////////////////////////////////////////
-        // theStack.push(input)
+        // PUSH - Insert a value to the top of the stack.
         //
         strValue = "Hello";
-        System.out.println(theStack.push(strValue)
+        System.out.println(theStack.push(strValue) // returns a boolean
                 ? "PUSH succeded: '" + strValue + "' was added to the stack."
                 : "PUSH failed: Stack is full.");
-        System.out.println(Arrays.toString(theStack.getStackArray()));
-        System.out.println();
+        print(theStack);
 
         strValue = "World";
-        System.out.println(theStack.push(strValue)
+        System.out.println(theStack.push(strValue) // returns a boolean
                 ? "PUSH succeded: '" + strValue + "' was added to the stack."
                 : "PUSH failed: Stack is full.");
-        System.out.println(Arrays.toString(theStack.getStackArray()));
-        System.out.println();
+        print(theStack);
 
         //////////////////////////////////////////////////////////////////
-        // theStack.pop()
+        // POP - Remove the value that is at the top of the stack.
         //
         strValue = theStack.pop();
-        System.out.println(strValue != null
+        System.out.println(strValue != null // returns popped string value
                 ? "POP succeded: '" + strValue + "' was removed from the stack."
                 : "POP failed: Sorry! Stack is empty, nothing to remove.");
-        System.out.println(Arrays.toString(theStack.getStackArray()));
-        System.out.println();
+        print(theStack);
 
         strValue = theStack.pop();
-        System.out.println(strValue != null
+        System.out.println(strValue != null // returns popped string value
                 ? "POP succeded: '" + strValue + "' was removed from the stack."
                 : "POP failed: Sorry! Stack is empty, nothing to remove.");
-        System.out.println(Arrays.toString(theStack.getStackArray()));
-        System.out.println();
+        print(theStack);
 
         //////////////////////////////////////////////////////////////////
-        // theStack.push(input)
+        // PUSH - Insert a value to the top of the stack.
         //
         strValue = "Hey";
-        System.out.println(theStack.push(strValue)
+        System.out.println(theStack.push(strValue) // returns a boolean
                 ? "PUSH succeded: '" + strValue + "' was added to the stack."
                 : "PUSH failed: Stack is full.");
-        System.out.println(Arrays.toString(theStack.getStackArray()));
-        System.out.println();
+        print(theStack);
 
         //////////////////////////////////////////////////////////////////
-        // theStack.push(input)
+        // PEEK - Just display the value that is at the top of the stack.
         //
-        System.out.println(theStack.peek() != null
+        System.out.println(theStack.peek() != null // returns peek string value
                 ? "PEEK succeded: '" + strValue + "' is at the top of the stack."
                 : "PEEK failed: Stack is empty.");
-        System.out.println(Arrays.toString(theStack.getStackArray()));
-        System.out.println();
+        print(theStack);
 
         //////////////////////////////////////////////////////////////////
-        // theStack.push(input)
+        // PUSH MULTIPLE - Insert many values to the stack
         //
         boolean succededPushingAll = theStack
                 .pushMany("good morning everybody I am here to tell you a little bit of myself");
-        System.out.println(succededPushingAll
+        System.out.println(succededPushingAll // returns a boolean
                 ? "PUSH succeded with all multiples values."
                 : "PUSH failed with some of the multiples values.");
-        System.out.println(Arrays.toString(theStack.getStackArray()));
-        System.out.println();
+        print(theStack);
 
+        //////////////////////////////////////////////////////////////////
+        // POP ALL - Remove all the values of the stack
+        //
         System.out.println("POP all values from stack.");
         theStack.popAll();
+        print(theStack);
+
+    }
+
+    // Print the stack and jump line
+    private static void print(TheStack theStack) {
         System.out.println(Arrays.toString(theStack.getStackArray()));
         System.out.println();
-
     }
 
 }
