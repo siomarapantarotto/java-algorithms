f54cd073e87ff6cd577ec51997b6d6a78f13058c
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2022-05-25 05:03:38 -0300

    Space complexity O(1) - Difficulty Level : Hard

--- geeksforgeeks/linkedlist/CloneLinkedListWithRandomNode.java
+++ geeksforgeeks/linkedlist/CloneLinkedListWithRandomNode.java
@@ -100,3 +100,15 @@ public class CloneLinkedListWithRandomNode {
         print(cloned_list);
     }
 }
+
+/**
+ * Difficulty Level : Hard
+ * 
+ * Given a linked list having two pointers in each node.
+ * The first one points to the next node of the list,
+ * however, the other pointer is random and can point
+ * to any node of the list. Write a program that clones
+ * the given list in O(1) space, without any extra space.
+ * 
+ * by Ashutosh Kumar 😀
+ */
