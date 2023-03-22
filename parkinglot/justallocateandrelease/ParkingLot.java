1c146d1f97aa0d8f64803d1189410f7a16f8b679
SIOMARA CINTIA PANTAROTTO <siomarapantarotto@gmail.com>
2023-03-22 19:48:54 -0300

    Initial commit

---
+++ parkinglot/justallocateandrelease/ParkingLot.java
@@ -0,0 +1,58 @@
+package parkinglot.justallocateandrelease;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ParkingLot {
+
+    private int totalCapacity;
+    private int availableCapacity;
+    private Map<String, Vehicle> parkingSpaces;
+
+    public ParkingLot(int totalCapacity) {
+        this.totalCapacity = totalCapacity;
+        this.availableCapacity = totalCapacity;
+        this.parkingSpaces = new HashMap<>();
+    }
+
+    public boolean allocateParkingSpace(Vehicle vehicle) {
+        if (this.availableCapacity > 0) {
+            this.parkingSpaces.put(vehicle.getLicensePlate(), vehicle);
+            this.availableCapacity--;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean releaseParkingSpace(Vehicle vehicle) {
+        if (this.parkingSpaces.containsKey(vehicle.getLicensePlate())) {
+            this.parkingSpaces.remove(vehicle.getLicensePlate());
+            this.availableCapacity++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getTotalCapacity() {
+        return totalCapacity;
+    }
+
+    public int getAvailableCapacity() {
+        return availableCapacity;
+    }
+
+    public Map<String, Vehicle> getParkingSpaces() {
+        return parkingSpaces;
+    }
+
+    public void print() {
+        // print the contents of the HashMap
+        for (String key : parkingSpaces.keySet()) {
+            System.out.println(key + ": " + Arrays.asList(parkingSpaces.get(key)));
+        }
+    }
+
+}
---
+++ parkinglot/justallocateandrelease/Vehicle.java
@@ -0,0 +1,17 @@
+package parkinglot.justallocateandrelease;
+
+public class Vehicle {
+    private String licensePlate;
+
+    public Vehicle(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+}
